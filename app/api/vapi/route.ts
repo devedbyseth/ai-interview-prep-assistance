e87cfccb32c667bcb1334a6912aceef4cb1b549a
Octo2Blazing <piseth@IISETH.local>
2025-06-30 22:47:20 +0700

    adjusting for deploy 2

--- app/api/vapi/route.ts
+++ app/api/vapi/route.ts
@@ -12,7 +12,18 @@ export async function GET(){
 export async function POST(req: Request) {
     const user = await getCurrentUser();
     const {role, level, techstack, type, amount} = await req.json();
-    const prompt = returnPrompt(role, level, techstack, type, amount);
+    const prompt = `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this: ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `
     if(user){
         const { text } = await generateText({
             model: google("gemini-2.0-flash"),
@@ -36,19 +47,4 @@ export async function POST(req: Request) {
         return Response.json({status: 500, message: "User not found"})
     }
 
-}
-
-const returnPrompt = (role: string, level:string, techstack:string[], type:string, amount:number) =>{
-    return `Prepare questions for a job interview.
-        The job role is ${role}.
-        The job experience level is ${level}.
-        The tech stack used in the job is: ${techstack}.
-        The focus between behavioural and technical questions should lean towards: ${type}.
-        The amount of questions required is: ${amount}.
-        Please return only the questions, without any additional text.
-        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
-        Return the questions formatted like this: ["Question 1", "Question 2", "Question 3"]
-        
-        Thank you! <3
-    `;
 }
